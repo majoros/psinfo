127d0cd4def6d19c68558f038a7af6e90bec8fd5
Chris Majoros <chris@majoros.us>
2012-02-10 17:37:48 -0500

    DEV: Its working more reliably now. DEV: added verbose levels and some debug output.

--- pargs.c
+++ pargs.c
@@ -1,18 +1,23 @@
 #include <link.h>
-#include <elf.h>
-#include <sys/ptrace.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <ctype.h>
-#include <unistd>
 #include <stdio.h>
 #include <errno.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <sys/ptrace.h>
+#include <libelf.h>
 
-#define ENVIRON environ;
+#define MAX_STR_LEN 4096;
 
-/* 
+#define LOG_DEBUG  0
+#define LOG_INFO   1
+#define LOG_ERROR  2
+#define MAX_MSG    256
+/*
  * search locations of DT_SYMTAB and DT_STRTAB and save them into global
  * variables, also save the nchains from hash table.
  */
@@ -21,17 +26,34 @@
  * Research:
  *    http://www.acsu.buffalo.edu/~charngda/elf.html
  *    http://www.ibm.com/developerworks/linux/library/l-dynamic-libraries/
+ *    http://www.symantec.com/connect/articles/dynamic-linking-linux-and-windows-part-one
  *
  */
 
+void logit( int msg_level, const char *fmt, ... );
+
+int log_level = LOG_ERROR;
+int aflag = 1;
+int cflag = 0;
+int eflag = 0;
+int lflag = 0;
+int xflag = 0;
+int Fflag = 0;
+int dflag = 0;
+
+int env;
+int _env;
+int __env;
+
 unsigned long symtab;
 unsigned long strtab;
-int nchains;
+
+int nchains = 0;
 
 /* attach to pid */
 void ptrace_attach(int pid) {
     if((ptrace(PTRACE_ATTACH, pid, NULL, NULL)) < 0) {
-        perror("ptrace_attach");
+        logit( LOG_ERROR, "ERROR: Unable to attach to [%i]: %i\n", pid, errno );
         exit(-1);
     }
     waitpid(pid, NULL, WUNTRACED);
@@ -40,40 +62,52 @@ void ptrace_attach(int pid) {
 /* detach process */
 void ptrace_detach(int pid) {
     if(ptrace(PTRACE_DETACH, pid, NULL, NULL) < 0) {
-        perror("ptrace_detach");
+        logit( LOG_ERROR, "ERROR: Unable to detach from [%i]: %i\n", pid, errno );
         exit(-1);
     }
 }
 
 /* read data from attached process's memory address */
 void read_data(int pid, unsigned long addr, void *vptr, int len){
-
     /* TODO: Check if we are going to go out on bounds and then do... somthing */
-
     int i, count;
     long word;
     unsigned long *ptr = (unsigned long *) vptr;
     count = i = 0;
     while (count < len) {
         word = ptrace(PTRACE_PEEKTEXT, pid, addr + count, NULL);
-        count += 4;
+        count += sizeof(int);
         ptr[i++] = word;
     }
 }
+
 /* read string from pid's memory */
-char *read_str(int pid, unsigned long addr, int len)
-{
-    char *ret = calloc(32, sizeof(char));
-    read_data(pid, addr, ret, len);
-    return ret;
+char *read_str(int pid, unsigned long addr, int max) {
+    int i, count;
+    long word;
+    char *str = calloc(max, sizeof(char));
+    count = i = 0;
+    while (count < max) {
+        word = ptrace(PTRACE_PEEKTEXT, pid, addr + count, NULL);
+        count += sizeof(char);
+        str[i++] = word;
+        if( (char)word == '\0'){
+            goto END;
+        }
+    }
+
+END:
+    return (char *) str;
 }
 
 /* locate link-map in pid's memory */
 struct link_map *locate_linkmap(int pid) {
+
     Elf32_Ehdr *ehdr = malloc(sizeof(Elf32_Ehdr));
     Elf32_Phdr *phdr = malloc(sizeof(Elf32_Phdr));
-    Elf32_Dyn *dyn = malloc(sizeof(Elf32_Dyn));
+    Elf32_Dyn  *dyn  = malloc(sizeof(Elf32_Dyn));
     Elf32_Word got;
+
     struct link_map *l = malloc(sizeof(struct link_map));
     unsigned long phdr_addr, dyn_addr, map_addr;
 
@@ -88,7 +122,19 @@ struct link_map *locate_linkmap(int pid) {
     read_data(pid, 0x08048000, ehdr, sizeof(Elf32_Ehdr));
     phdr_addr = 0x08048000 + ehdr->e_phoff;
 
-    /* printf("program header at %p\n",(void *) phdr_addr); */
+
+    /*
+     * Just making sure we have the actual elf header
+     */
+    if( (int)ehdr->e_ident[0] != 0x7f ||
+        (int)ehdr->e_ident[1] != 'E'  ||
+        (int)ehdr->e_ident[2] != 'L'  ||
+        (int)ehdr->e_ident[3] != 'F' ){
+        logit( LOG_ERROR, "Can't find a proper elf header\n");
+        return;
+    }
+
+    logit( LOG_DEBUG, "DEBUG: Program header at %p %c\n",(void *) phdr_addr, ehdr->e_ident[1]);
 
     read_data(pid, phdr_addr, phdr, sizeof(Elf32_Phdr));
 
@@ -109,18 +155,20 @@ struct link_map *locate_linkmap(int pid) {
     }
 
     /*
-     * TODO: Need to find a way to make sure what im getting is the actual GOT
-     * and that i have not been going through some random memory addresses
+     * TODO: Need to find a way to make sure what i'm getting is the actual GOT
+     * and that I have not been going through some random memory addresses
      * getting who knows what.
      */
 
     got = (Elf32_Word) dyn->d_un.d_ptr;
-    got += 4;   /* second GOT entry */
+    got += sizeof(int);  /*  second GOT entry */
+    logit( LOG_DEBUG, "DEBUG: The GOT address is %p\n", got);
+    /* we got the GOT! */
 
     /*
      * Read first link_map item and return it
      */
-    read_data(pid, (unsigned long) got, &map_addr, 4);
+    read_data(pid, (unsigned long) got, &map_addr, sizeof(int));
     read_data(pid, map_addr, l, sizeof(struct link_map));
     /* again how do i know i am getting what i want. */
 
@@ -131,25 +179,29 @@ struct link_map *locate_linkmap(int pid) {
 }
 
 /* resolve the tables for symbols*/
-void resolv_tables(int pid, struct link_map *map)
-{
+void resolv_tables(int pid, struct link_map *map) {
+
     Elf32_Dyn *dyn = malloc(sizeof(Elf32_Dyn));
+
     unsigned long addr;
+
     addr = (unsigned long) map->l_ld;
+
     read_data(pid, addr, dyn, sizeof(Elf32_Dyn));
+
     while (dyn->d_tag) {
         switch (dyn->d_tag) {
         case DT_HASH:
-            read_data( pid,
-                       dyn->d_un.d_ptr + map->l_addr + 4,
-                       &nchains,
-                       sizeof(nchains)
-                     );
+            read_data(pid, dyn->d_un.d_ptr +
+                  map->l_addr + 4, &nchains,
+                  sizeof(nchains));
             break;
         case DT_STRTAB:
+            logit( LOG_DEBUG, "DEBUG: strtab address: %i\n", dyn->d_tag);
             strtab = dyn->d_un.d_ptr;
             break;
         case DT_SYMTAB:
+            logit( LOG_DEBUG, "DEBUG: symtab address: %i\n", dyn->d_tag);
             symtab = dyn->d_un.d_ptr;
             break;
         default:
@@ -162,66 +214,125 @@ void resolv_tables(int pid, struct link_map *map)
 }
 
 /* find symbol in DT_SYMTAB */
-int find_sym_in_tables(int pid, struct link_map *map)
-{
+void find_environ_sym(int pid, struct link_map *map) {
     Elf32_Sym *sym = malloc(sizeof(Elf32_Sym));
     char *str;
-    int env;
     int val;
     int i = 0;
-    int f = 0;
 
-    while (i < nchains) {
+    do{
         read_data(pid, symtab + (i * sizeof(Elf32_Sym)), sym,
               sizeof(Elf32_Sym));
         i++;
 
-        str = read_str(pid, strtab + sym->st_name, 8);
-        /* compare it with our symbol*/
+        str = read_str(pid, strtab + sym->st_name, 64);
+        /* compare it with our symbol
+        printf("size: %i str: %s  %i\n", sym->st_info, str, i);
+        */
+
         if (strcmp(str, "environ") == 0) {
-            f = 1;
-
-            /* we have the address to the symbol of environ. now we want what
-             * the actual environ pointer
-             */
-            read_data(pid, ( map->l_addr + sym->st_value), &env, 4);
-
-            /*
-             * now we can FINALLY loop through the aray of environment
-             * variables. Get the first one and then loop untill nil
-             */
-
-            read_data(pid, env, &val, 4);
-            while( val != 0 ){
-                str = read_str(pid, val, 32);
-                printf( "%s\n", str);
-                env += 4;
-                read_data(pid, env, &val, 4);
-            }
+            env = (map->l_addr + sym->st_value);
+            logit( LOG_DEBUG, "DEBUG: environ symbol address: %p\n", env);
         }
-    }
-    return f;
+        else if (strcmp(str, "_environ") == 0) {
+            _env = (map->l_addr + sym->st_value);
+            logit( LOG_DEBUG, "DEBUG: _environ symbol address: %p\n", _env);
+        }
+        else if (strcmp(str, "__environ") == 0) {
+            __env = (map->l_addr + sym->st_value);
+            logit( LOG_DEBUG, "DEBUG: __environ symbol address: %p\n", __env);
+        }
+
+    }while( (int)str[0] != -1 ); /* FIXME: */
+
+    return;
+
 }
 
-int main(int argc, char *argv[])
-{
+void show_environ( int pid ){
 
-    int c;
+    char *str;
+    int sym_addr;
+    int env_addr;
+    int str_addr;
+    int f = 0;
 
-    int aflag = 1;
-    int cflag = 0;
-    int eflag = 0;
-    int lflag = 0;
-    int xflag = 0;
-    int Fflag = 0;
+    /* TODO: hhhmmm how many of these can there be? */
+    if( (int*)__env != NULL ){
+        sym_addr = __env;
+    }
 
-    int pid;
+    if( (int*)_env != NULL ){
+        sym_addr = _env;
+    }
+
+    if( (int*)env != NULL ){
+        sym_addr = env;
+    }
+
+    if( (int*)sym_addr == NULL ){
+        logit( LOG_ERROR, "ERROR: Unable to find an environ symbol\n");
+        return;
+    }
+    logit( LOG_DEBUG, "DEBUG: The environ symbol address: %p\n", sym_addr );
+
+    /* we only have the address to the symbol of environ. Now we want
+     * the actual environ pointer
+     */
+    read_data(pid, sym_addr, &env_addr, sizeof(int));
+
+    logit( LOG_DEBUG, "DEBUG: The environ array address: %p\n", env_addr);
+
+    /*
+     * now we can FINALLY loop through the array of environment
+     * variables. Get the first one and then loop until nil
+     */
+    read_data(pid, env_addr, &str_addr, sizeof(int));
+
+    int i = 0;
+    do{
+        i++;
+        str = read_str(pid, str_addr, 4096 );
+        printf( "%s\n", str);
+
+        env_addr += sizeof(int);
+        read_data(pid, env_addr, &str_addr, sizeof(int));
+    } while( str_addr != 0 ); /* TODO: zero ??? */
+}
+
+void usage() {
+}
+
+void display_args() {
+}
+
+void display_env( int pid ){
     int ret;
-    unsigned long *value;
     struct link_map *lm;
 
-    while (( c = getopt( argc, argc, "acelxF:" )) != -1 ){
-        switch (c){
+    ptrace_attach(pid);
+
+    lm = locate_linkmap(pid);
+
+    resolv_tables(pid, lm);
+
+    find_environ_sym(pid, lm);
+
+    show_environ(pid);
+
+    ptrace_detach(pid);
+}
+
+int main(int argc, char **argv) {
+
+    int c;
+    int pid;
+
+    opterr = 0;
+
+    while (( c = getopt( argc, argv, "acelxvF:" )) != -1 )
+        switch (c)
+        {
             case 'a':
                 aflag = 1;
                 break;
@@ -240,23 +351,40 @@ int main(int argc, char *argv[])
             case 'F':
                 Fflag = 1;
                 break;
+            case 'd':
+                dflag = 1;
+                break;
+            case 'v':
+                log_level--;
+                printf("log level %i\n", log_level);
+                break;
+            default:
+                usage();
         }
+
+    if( argc == optind ){
+        logit( LOG_ERROR, "You must supply a single pid\n" );
+        exit(1);
     }
 
-    if( argc != 1 ){
-        fprintf( stderr, "You must supply a single pid\n");
-        exit 1;
+    pid = atoi( argv[optind] );
+
+    if( aflag )
+        display_args(pid);
+
+    if( eflag )
+        display_env(pid);
+}
+
+void logit( int msg_level, const char *fmt, ... ){
+
+    va_list ap;
+    char msg[MAX_MSG];
+
+    if( msg_level >= log_level ){
+        va_start(ap, fmt);
+        vfprintf( stderr, fmt, ap );
+        va_end(ap);
     }
-
-    pid = atoi(argv[1]);
-    ptrace_attach(pid);
-
-    lm = locate_linkmap(pid);
-    resolv_tables(pid, lm);
-
-    ret = find_sym_in_tables(pid, lm);
-
-    ptrace_detach(pid);
-    return 0;
 }
 
